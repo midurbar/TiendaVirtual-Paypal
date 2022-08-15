62f1ac6816563245c8ed38110e719b85a7e9a912
midurbar <midurbar@gmail.com>
2022-08-15 18:19:04 +0200

    Creada carpeta config y config.php

---
+++ Config/config.php
@@ -0,0 +1,11 @@
+<?php
+    const BASE_URL = "http://localhost/eccommerce/";
+    const HOST = "localhost";
+    const USER = "root";
+    const PASS = "";
+    const DB = "carrito";
+    const CHARSET = "charset=utf8";
+    const TITLE = "TIENDA ONLINE";
+    const MONEDA = "USD";
+    const CLIENT_ID = "";
+    ?>
