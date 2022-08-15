687f92cc51096476b54819ec468361f07bd8445d
midurbar <midurbar@gmail.com>
2022-08-15 19:14:18 +0200

    editado config

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,5 @@
 <?php
-    const BASE_URL = "http://localhost/tienda-virtual/";
+    const BASE_URL = "http://localhost/TiendaVirtual-Paypal/";
     const HOST = "localhost";
     const USER = "root";
     const PASS = "";
