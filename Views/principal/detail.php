8ecef04b86014c665bb30d5a7a006748cf9dcb47
midurbar <midurbar@gmail.com>
2022-12-05 20:43:57 +0100

    editado detail.php y shop.php

--- Views/principal/detail.php
+++ Views/principal/detail.php
@@ -163,17 +163,17 @@
                             <div
                                 class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
                                 <ul class="list-unstyled">
-                                    <li><a class="btn btn-util text-white" href="shop-single.html"><i
+                                    <li><a class="btn btn-util text-white" href="#"><i
                                                 class="fas fa-heart"></i></a></li>
                                     <li><a class="btn btn-util text-white mt-2" href="<?php echo BASE_URL . 'principal/detail/' . $relacion['id']; ?>"><i
                                                 class="fas fa-eye"></i></a></li>
-                                    <li><a class="btn btn-util text-white mt-2" href="shop-single.html"><i
+                                    <li><a class="btn btn-util text-white mt-2" href="#"><i
                                                 class="fas fa-cart-plus"></i></a></li>
                                 </ul>
                             </div>
                         </div>
                         <div class="card-body">
-                            <a href="shop-single.html" class="h3 text-decoration-none"><?php echo $relacion['nombre']; ?></a>
+                            <a href="<?php echo BASE_URL . 'principal/detail/' . $relacion['id']; ?>" class="h3 text-decoration-none"><?php echo $relacion['nombre']; ?></a>
                             <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
                                 
                                 <li class="pt-2">
