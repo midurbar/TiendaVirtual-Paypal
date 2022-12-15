d2026b2dc7bb6ebf8c0ad10567b4c7288f664cda
midurbar <midurbar@gmail.com>
2022-12-15 19:53:39 +0100

    añadido sweetalert2 y editado carrito.js

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -1,7 +1,8 @@
-const btnAddDeseo = document.querySelectorAll('btnAddDeseo');
+const btnAddDeseo = document.querySelectorAll('.btnAddDeseo');
 const btnDeseo = document.querySelector('#btnCantidadDeseo');
 let listaDeseo= [];
 document.addEventListener('DOMContentLoaded', function() {
+    cantidadDeseo();
     for (let i=0; i<btnAddDeseo.length; i++) {
         btnAddDeseo[i].addEventListener('click', function() {
             let idProducto = btnAddDeseo[i].getAttribute('prod');
@@ -16,10 +17,19 @@ function agregarDeseo(idProducto){
         "cantidad": 1
     })
     localStorage.setItem('listaDeseo', JSON.stringify(listaDeseo));
+    Swal.fire(
+        'Aviso',
+        'PRODUCTO AGREGADO A LA LISTA DE DESEOS',
+        'success'
+    )
     cantidadDeseo();
 }
 
 function cantidadDeseo() {
     let listas=JSON.parse(localStorage.getItem('listaDeseo'));
-    console.log(listas);
+    if (listas!=null) {
+        btnDeseo.textContent=listas.length;
+    }else {
+        btnDeseo.textContent=0;
+    }
 }
