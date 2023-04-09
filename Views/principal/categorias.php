272ee9435fa737d920d39935111fb395b1dfaf01
midurbar <midurbar@gmail.com>
2023-04-09 20:41:53 +0200

    editadas vistas de categoria y detalles

--- Views/principal/categorias.php
+++ Views/principal/categorias.php
@@ -26,11 +26,18 @@
                                 <div
                                     class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
                                     <ul class="list-unstyled">
-                                        <li><a class="btn btn-util text-white btnAddDeseo" href="#" prod="<?php echo $producto['id']; ?>"><i
-                                                    class="fas fa-heart"></i></a></li>
-                                        <li><a class="btn btn-util text-white mt-2" href="<?php echo BASE_URL . 'principal/detail/' . $producto['id']; ?>"><i
-                                                    class="fas fa-eye"></i></a></li>
-                                        <li><a class="btn btn-util text-white mt-2 btnAddCarrito" href="#" prod="<?php echo $producto['id']; ?>"><i
+                                        <li>
+                                            <a class="btn btn-util text-white btnAddDeseo" href="#" prod="<?php echo $producto['id']; ?>"><i
+                                                    class="fas fa-heart"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="btn btn-util text-white mt-2" href="<?php echo BASE_URL . 'principal/detail/' . $producto['id']; ?>"><i
+                                                    class="fas fa-eye"></i>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a class="btn btn-util text-white mt-2 btnAddCarrito" href="#" prod="<?php echo $producto['id']; ?>"><i
                                                     class="fas fa-cart-plus"></i>
                                             </a>
                                         </li>
--- Views/principal/detail.php
+++ Views/principal/detail.php
@@ -154,26 +154,35 @@
             <!--Start Carousel Wrapper-->
             <div id="carousel-related-product">
 
-                <?php foreach ($data['relacionados'] as $relacion) { ?> 
+                <?php foreach ($data['relacionados'] as $producto) { ?> 
                 
                     <div class="p-2 pb-3">
                     <div class="product-wap card rounded-0">
                         <div class="card rounded-0">
-                            <img class="card-img rounded-0 img-fluid" src="<?php echo $relacion['imagen']; ?>">
+                            <img class="card-img rounded-0 img-fluid" src="<?php echo $producto['imagen']; ?>">
                             <div
                                 class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
                                 <ul class="list-unstyled">
-                                    <li><a class="btn btn-util text-white" href="#"><i
-                                                class="fas fa-heart"></i></a></li>
-                                    <li><a class="btn btn-util text-white mt-2" href="<?php echo BASE_URL . 'principal/detail/' . $relacion['id']; ?>"><i
-                                                class="fas fa-eye"></i></a></li>
-                                    <li><a class="btn btn-util text-white mt-2" href="#"><i
-                                                class="fas fa-cart-plus"></i></a></li>
+                                    <li>
+                                        <a class="btn btn-util text-white btnAddDeseo" href="#" prod="<?php echo $producto['id']; ?>"><i
+                                                class="fas fa-heart"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="btn btn-util text-white mt-2" href="<?php echo BASE_URL . 'principal/detail/' . $producto['id']; ?>"><i
+                                                class="fas fa-eye"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="btn btn-util text-white mt-2 btnAddCarrito" href="#" prod="<?php echo $producto['id']; ?>"><i
+                                                class="fas fa-cart-plus"></i>
+                                        </a>
+                                    </li>
                                 </ul>
                             </div>
                         </div>
                         <div class="card-body">
-                            <a href="<?php echo BASE_URL . 'principal/detail/' . $relacion['id']; ?>" class="h3 text-decoration-none"><?php echo $relacion['nombre']; ?></a>
+                            <a href="<?php echo BASE_URL . 'principal/detail/' . $producto['id']; ?>" class="h3 text-decoration-none"><?php echo $producto['nombre']; ?></a>
                             <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
                                 
                                 <li class="pt-2">
@@ -197,7 +206,7 @@
                                     <i class="text-muted fa fa-star"></i>
                                 </li>
                             </ul>
-                            <p class="text-center mb-0"><?php echo $relacion['precio'] . ' ' . MONEDA; ?></p>
+                            <p class="text-center mb-0"><?php echo $producto['precio'] . ' ' . MONEDA; ?></p>
                         </div>
                     </div>
                     </div>
