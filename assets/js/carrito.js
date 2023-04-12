4349e1126b341c24bd69abeb5cdafa92f0a047b9
midurbar <midurbar@gmail.com>
2023-04-12 15:53:13 +0200

    Revert "editadas vistas de categoria y detalles"
    This reverts commit 272ee9435fa737d920d39935111fb395b1dfaf01.

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -8,14 +8,14 @@ document.addEventListener('DOMContentLoaded', function() {
     if (localStorage.getItem('listaDeseo') != null) {
         listaDeseo=JSON.parse(localStorage.getItem('listaDeseo'));
     }
-    if (localStorage.getItem('listaCarrito') != null) {
-        listaCarrito=JSON.parse(localStorage.getItem('listaCarrito'));
-    }
     for (let i=0; i<btnAddDeseo.length; i++) {
         btnAddDeseo[i].addEventListener('click', function() {
             let idProducto = btnAddDeseo[i].getAttribute('prod');
             agregarDeseo(idProducto);
         });
+    }
+    if (localStorage.getItem('listaCarrito') != null) {
+        listaCarrito=JSON.parse(localStorage.getItem('listaCarrito'));
     }
     for (let i = 0; i < btnAddCarrito.length; i++) {
         btnAddCarrito[i].addEventListener('click', function() {
